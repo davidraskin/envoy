cb01b16f48fae1eeadd2862f1a9a2c8d5e943e46
davidraskin <draskin@google.com>
2020-07-16 20:32:18 +0000

    Implement metadata access log filter

--- source/common/access_log/access_log_impl.h
+++ source/common/access_log/access_log_impl.h
@@ -15,6 +15,7 @@
 #include "common/grpc/status.h"
 #include "common/http/header_utility.h"
 #include "common/protobuf/protobuf.h"
+#include "common/common/matchers.h"
 
 #include "absl/hash/hash.h"
 
@@ -229,12 +230,20 @@ private:
 };
 
 /**
-  * Filters requests based on value of 'access_log_policy' key
+  * Filters requests based on dynamic metadata
   */
-class LogKeyFilter : public Filter {
-    bool evaluate(const StreamInfo::StreamInfo& info, const Http::RequestHeaderMap& request_headers,
-                const Http::ResponseHeaderMap& response_headers,
-                const Http::ResponseTrailerMap& response_trailers) const override;
+class MetadataFilter : public Filter {
+public:
+  MetadataFilter(const envoy::config::accesslog::v3::MetadataFilter& config);
+
+  bool evaluate(const StreamInfo::StreamInfo& info, const Http::RequestHeaderMap& request_headers,
+              const Http::ResponseHeaderMap& response_headers,
+              const Http::ResponseTrailerMap& response_trailers) const override;
+
+private:
+  const Envoy::Matchers::MetadataMatcher matcher_;
+
+  
 };
 
 /**
